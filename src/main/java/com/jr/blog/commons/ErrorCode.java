e1411ceab14e1ed98813234388c7a5b894f78297
JR <3142734033@qq.com>
2022-06-24 00:08:41 +0800

    自定义异常类和全局异常处理

--- src/main/java/com/jr/blog/commons/ErrorCode.java
+++ src/main/java/com/jr/blog/commons/ErrorCode.java
@@ -5,10 +5,14 @@ package com.jr.blog.commons;
  */
 public enum ErrorCode {
     SUCCESS(20000,"success", ""),
-    NOT_FOUND(40400,"为找到该资源", ""),
+    NOT_FOUND(40400,"未找到该资源", ""),
     INTERNAL_SERVER_ERROR(50000, "服务器内部错误!", ""),
     SERVER_BUSY(50300,"服务器正忙，请稍后再试!", ""),
-    SYSTEM_ERROR(50000,"系统内部异常","");
+    SYSTEM_ERROR(50000,"系统内部异常",""),
+    SIGNATURE_ERROR(40101,"token签名不一致",""),
+    TOKEN_EXPIRED_ERROR(40102,"token令牌过期",""),
+    ALGORITHM_MISMATCH_ERROR(40103,"token算法不匹配",""),
+    INVALID_CLAIM(40104,"token的payload失效","");
 
     private final int code;
 
